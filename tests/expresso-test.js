621519c45d4ebed58115c102bad85c47946b6021
Puyan Wei <puyanwei@gmail.com>
2017-12-15 16:15:45 +0000

    added first passing test for notes

--- tests/expresso-test.js
+++ tests/expresso-test.js
@@ -3,8 +3,3 @@ describe("#testing-expresso", function() {
   expect.isNull("testing that null is indeed null", null);
   expect.isTrue("testing that one plus one is two", 1 + 1 == 2);
 });
-
-describe("#initialize", function() {
-  notes = new Notes();
-  expect.isEqual("array is zero length", notes.list.length, 0);
-});
--- tests/note-model-test.js
+++ tests/note-model-test.js
@@ -1,3 +1,6 @@
-// notes = new Notes();
-// expect.isEqual("creates an empty list for notes", notes.list.length, 0);
+notes = new Notes();
+
+describe("#initialize", function() {
+  expect.isEqual("blank array is loaded", notes.list.length, 0);
+});
 // expect.isEqual("adds a note to the list", notes.addNote(), "hello");
