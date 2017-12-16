e706cdc4be849d361845b88aae96846cfb69ecb3
Puyan Wei <puyanwei@gmail.com>
2017-12-16 12:15:18 +0000

    created new class Note, added new tests for list

--- tests/model-tests.js
+++ tests/model-tests.js
@@ -3,11 +3,19 @@ describe("#initialize", function() {
   expect.isEqual("blank array is loaded", list.notelist.length, 0);
 });
 
-describe("#create", function() {
+describe("#add", function() {
   var list = new List();
   list.add("hello");
-  expect.isEqual("can create and store a note", list.notelist[0], "hello");
+  expect.isEqual(
+    "can create and store a note",
+    list.notelist[0].view(),
+    "hello"
+  );
   list.add("goodbye");
-  expect.isEqual("second note says goodbye", list.notelist[1], "goodbye");
+  expect.isEqual(
+    "second note says goodbye",
+    list.notelist[1].view(),
+    "goodbye"
+  );
   expect.isEqual("there are two list in the list", list.notelist.length, 2);
 });
