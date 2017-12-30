0dec4ee5617d4b01a1f522eacde22fb127fedfd4
Puyan Wei <puyanwei@gmail.com>
2017-12-30 15:37:17 +0000

    mocked test for outputHTML method in controller, had to create fake tag and also remove it after

--- tests/controller-tests.js
+++ tests/controller-tests.js
@@ -4,12 +4,33 @@ describe("NoteController", () => {
     var listView = {
       converted: "<ul><li><div>hello this is a note</div></li></ul>"
     };
-    var noteController = new NoteController(list, listView);
+    var noteController = new NoteController(list, listView, "app");
 
     expect.isEqual(
       "gets the list view",
       noteController.getListView(),
       "<ul><li><div>hello this is a note</div></li></ul>"
     );
+  });
+
+  describe("#outputHTML", () => {
+    var list = { text: "hello this is another note" };
+    var listView = {
+      converted: "<ul><li><div>hello this is another note</div></li></ul>"
+    };
+    var body = document.getElementsByTagName("body");
+    var mockElement = document.createElement("span");
+    mockElement.id = "test";
+    body.item(0).appendChild(mockElement);
+
+    var noteController = new NoteController(list, listView, "test");
+
+    expect.isEqual(
+      "outputs the note as HTML to the page",
+      noteController.outputHTML(),
+      "<ul><li><div>hello this is another note</div></li></ul>"
+    );
+
+    body.item(0).removeChild(mockElement);
   });
 });
