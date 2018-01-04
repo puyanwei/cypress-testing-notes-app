d1b57181409933863e13ed9b213d1f8e92b7d089
Puyan Wei <puyanwei@gmail.com>
2018-01-04 14:41:21 +0000

    added failing test for note view in controller

--- tests/controller-tests.js
+++ tests/controller-tests.js
@@ -21,10 +21,32 @@ describe("Controller", () => {
       "<ul><li><div><a href='http://localhost:8080#notes/0'>hello this is a note</a></div></li><li><div><a href='http://localhost:8080#notes/1'>this is a second not</a></div></li></ul>"
     );
   });
-  //
-  // describe("#getNoteView", () => {
-  //   expect.isEqual("gets the note view", controller.getNoteView(), "");
-  // });
+
+  describe("#getNoteView", () => {
+    var list = [
+      {
+        text: "hello this is a note"
+      },
+      {
+        text: "this is a second note that is longer"
+      }
+    ];
+    var listView = {
+      converted:
+        "<ul><li><div><a href='http://localhost:8080#notes/0'>hello this is a note</a></div></li><li><div><a href='http://localhost:8080#notes/1'>this is a second not</a></div></li></ul>"
+    };
+    var noteView = {
+      convert: "<div>this is a second note that is longer</div>"
+    };
+
+    var controller = new Controller(list, listView, "app");
+
+    expect.isEqual(
+      "gets the first note view",
+      controller.getNoteView(0),
+      "<div>this is a second note that is longer</div>"
+    );
+  });
 
   describe("#outputHTML", () => {
     var list = { text: "hello this is another note" };
@@ -43,6 +65,10 @@ describe("Controller", () => {
       controller.outputHTML(),
       "<ul><li><div>hello this is another note</div></li></ul>"
     );
+
+    body.item(0).removeChild(mockElement);
+  });
+});
 
     body.item(0).removeChild(mockElement);
   });
