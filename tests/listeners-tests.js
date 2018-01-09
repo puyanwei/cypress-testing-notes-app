6614776f24180ebc08d48bb8bbed8a1d7abb0b4e
Puyan Wei <puyanwei@gmail.com>
2018-01-09 18:50:47 +0000

    unfinished test, need to create mock element for it

--- tests/listeners-tests.js
+++ tests/listeners-tests.js
@@ -1,10 +1,16 @@
 describe("Listeners", () => {
   describe("#addTextToList", () => {
-    var text = { elements: [{ value: "hello" }, 2] };
+    // var text = { elements: [{ value: "hello" }, 2] };
+    var body = document.getElementsByTagName("body");
+    var mockElement = document.createElement("span");
+    mockElement.id = "text";
+    mockElement.value = "hello";
+    body.item(0).appendChild(mockElement);
+
     var list = [{ add: "" }];
-    var listView = {};
 
     listeners = new Listeners(list);
+    console.log(text.elements[0].value);
     listeners.addTextToList(list);
     expect.isEqual("gets the text from the text box", listeners.list, "hello");
   });
