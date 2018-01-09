21e9fae14b2b98662c7fa08ad6bf44c5fc20172b
Puyan Wei <puyanwei@gmail.com>
2018-01-09 18:36:52 +0000

    refactored to remove unecessary list parameters

--- tests/listeners-tests.js
+++ tests/listeners-tests.js
@@ -1,3 +1,11 @@
 describe("Listeners", () => {
-  describe("#getText", () => {});
+  describe("#addTextToList", () => {
+    var text = { elements: [{ value: "hello" }, 2] };
+    var list = [{ add: "" }];
+    var listView = {};
+
+    listeners = new Listeners(list);
+    listeners.addTextToList(list);
+    expect.isEqual("gets the text from the text box", listeners.list, "hello");
+  });
 });
