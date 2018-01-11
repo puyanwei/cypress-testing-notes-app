836b22b0edb54390813df0416eedd468de4d0be8
Puyan Wei <puyanwei@gmail.com>
2018-01-11 15:43:16 +0000

    created a mock element for my form and button

--- tests/listeners-tests.js
+++ tests/listeners-tests.js
@@ -1,17 +1,21 @@
 describe("Listeners", () => {
   describe("#addTextToList", () => {
-    // var text = { elements: [{ value: "hello" }, 2] };
     var body = document.getElementsByTagName("body");
-    var mockElement = document.createElement("span");
-    mockElement.id = "text";
-    mockElement.value = "hello";
-    body.item(0).appendChild(mockElement);
+    var mockForm = document.createElement("form");
+    mockForm.id = "text";
+    var mockTextarea = document.createElement("textarea");
+    var mockButton = document.createElement("input");
+    mockButton.setAttribute("type", "submit");
+    mockForm.appendChild(mockTextarea);
+    mockForm.appendChild(mockButton);
+    body.item(0).appendChild(mockForm);
 
-    var list = [{ add: "" }];
+    var list = { add: "" };
 
     listeners = new Listeners(list);
-    console.log(text.elements[0].value);
     listeners.addTextToList(list);
+    console.log(noteText);
+    console.log(list);
     expect.isEqual("gets the text from the text box", listeners.list, "hello");
   });
 });
