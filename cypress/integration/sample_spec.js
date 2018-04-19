4a3f9ca711d120106635136fa2aef43a34608235
Puyan Wei <puyanwei@gmail.com>
2018-04-19 11:08:22 +0100

    wrote passing test

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,6 @@
+describe('My first Cypress test!', () => {
+    it('Does not do at lot at all!', () => {
+        expect(true).to.equal(true);
+        expect(true).to.not.equal(false);
+    });
+});
